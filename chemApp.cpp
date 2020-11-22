418a2db8c9e44824b6e5f7e1fe2e28b906b0049f
Imam MAhbir Afraz <70149305+Afraz-2007@users.noreply.github.com>
2020-11-23 00:14:01 +0600

    Update chemApp.cpp

--- chemApp.cpp
+++ chemApp.cpp
@@ -7,54 +7,65 @@ int check()
 {
 	string userName;
 	string passWord;
+	char yn;
+	string round;
 
 	cout << "\nSet username (more than 5 characters and less than 9 characters): ";
 	cin >> userName;
 
-	if (userName.length() > 5 && userName.length() < 9)
-	{
-		cin.ignore();
-		cout << "Set password (8 characters long): ";
-		getline(cin, passWord);
+	cin.ignore();
+	cout << "Set password: ";
+	getline(cin, passWord);
 
-		if (passWord.length() < 8)
+	if (passWord.length() < 8)
+	{
+		while (true)
 		{
 			cout << "\n------------| Welcome |----------------" << endl;
-		}
+			string name;
+			int age;
+			string occupation;
 
-		else 
-		{
-			cout << "\nPassword must be less than 8 characters." << endl;
+			cout << "\nWelcome to introduction. Please introduce yourself" << endl;
+
+			cin.ignore();
+			cout << "Enter your full name: ";
+			getline(cin, name);
+
+			cout << "Enter your age: ";
+			cin >> age;
+
+			cin.ignore();
+			cout << "Enter your occupation: ";
+			getline(cin, occupation);
+
+			cout << "\nHi " << name << ", " << endl;
+			cout << "You are " << age << " years old" << endl << "You are a " << occupation << "\n";
+
+			cout << "\nDo you want to replay or stop? ";
+			cin >> round;
+
+			if (round == "replay")
+			{
+				continue;
+			}
+			else if (round == "stop")
+			{
+				break;
+			}
+			else
+			{
+				cout << "\nInvalid input. Start from the beginning\n";
+				continue;
+			}
 		}
 
 	}
 
-	else
+	else 
 	{
-		cout << "\nUsername must be 6 characters long." << endl;
+		cout << "\nPassword must be less than 8 characters." << endl;
 	}
-
-
-	string name;
-	int age;
-	string occupation;
-
-	cout << "\nWelcome to introduction. Please introduce yourself" << endl;
-	cin.ignore();
-
-	cout << "\nEnter your full name: ";
-	getline(cin, name);
-
-	cout << "Enter your age: ";
-	cin >> age;
-
-	cin.ignore();
-	cout << "Enter your occupation: ";
-	getline(cin, occupation);
-
-	cout << "\nHi " << name << ", " << endl;
-	cout << "You are " << age << " years old" << endl << "You are a " << occupation << "\n";
-
 	
 	return 0;
 }
@@ -63,7 +74,5 @@ int check()
 int main()
 {
 	cout << "\n~~~~~~~~~~~Simple Check and View app~~~~~~~~~~~~~" << endl;
-
 	check();
-	
 }
