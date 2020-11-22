04499bf0de8c43135867d6133a83a0822d8dbf46
Imam MAhbir Afraz <70149305+Afraz-2007@users.noreply.github.com>
2020-11-22 10:55:59 +0600

    Update project1.cpp

--- project1.cpp
+++ project1.cpp
@@ -27,29 +27,99 @@ int main()
 	int Num1;
 	int Num2;
 	char op;
-	cout << "\tEnter operator: ";
-	cin >> op;
-	cout << "\tEnter first number: ";
-	cin >> Num1;
-	cout << "\tEnter second number: ";
-	cin >> Num2;
-	if (op == '+')
-	{
-		cout << "\t" << Num1 << " + " << Num2 << " = " << add(Num1, Num2);
-	}
-	else if (op == '-')
-	{
-		cout << "\t" << Num1 << " - " << Num2 << " = " << subtract(Num1, Num2);
-	}
-	else if (op == '*')
-	{
-		cout << "\t" << Num1 << " * " << Num2 << " = " << multiply(Num1, Num2);
-	}
-	else if (op == '/')
-	{
-		cout << "\t" << Num1 << " / " << Num2 << " = " << divide(Num1, Num2);
-	}
+	char choice;
 
+	while (true)
+	{
+		cout << "\n:::::::::BASIC CALCULATOR::::::::::" << endl << "\n::::::::Operators:|+|-|*|/|::::::::" << endl;
+		cout << "\n\tEnter operator: ";
+		cin >> op;
+		cout << "\tEnter first number: ";
+		cin >> Num1;
+		cout << "\tEnter second number: ";
+		cin >> Num2;
+		if (op == '+')
+		{
+			cout << "\t" << Num1 << " + " << Num2 << " = " << add(Num1, Num2);
+			cout << "\n\tDo You Want To Continue? y/n: ";
+			cin >> choice;
+			if (choice == 'y')
+			{
+				continue;
+			}
+			else if (choice == 'n')
+			{
+				cout << "\n:::::::::END OF PROGRAM::::::::::::\n" << endl;
+				break;
+			}
+			else
+			{
+				cout << "\n\tInvalid input. Please Check!.\n" << endl;
+				continue;
+			}
+		}
+		else if (op == '-')
+		{
+			cout << "\t" << Num1 << " - " << Num2 << " = " << subtract(Num1, Num2);
+			cout << "\n\tDo You Want To Continue? y/n: ";
+			cin >> choice;
+			if (choice == 'y')
+			{
+				continue;
+			}
+			else if (choice == 'n')
+			{
+				cout << "\n:::::::::END OF PROGRAM::::::::::::\n" << endl;
+				break;
+			}
+			else
+			{
+				cout << "\n\tInvalid input. Please Check!.\n" << endl;
+				continue;
+			}
+		}
+		else if (op == '*')
+		{
+			cout << "\t" << Num1 << " * " << Num2 << " = " << multiply(Num1, Num2);
+			cout << "\n\tDo You Want To Continue? y/n: ";
+			cin >> choice;
+			if (choice == 'y')
+			{
+				continue;
+			}
+			else if (choice == 'n')
+			{
+				cout << "\n:::::::::END OF PROGRAM::::::::::::\n" << endl;
+				break;
+			}
+			else
+			{
+				cout << "\n\tInvalid input. Please Check!.\n" << endl;
+				continue;
+			}
+		}
+		else if (op == '/')
+		{
+			cout << "\t" << Num1 << " / " << Num2 << " = " << divide(Num1, Num2);
+			cout << "\n\tDo You Want To Continue? y/n: ";
+			cin >> choice;
+			if (choice == 'y')
+			{
+				continue;
+			}
+			else if (choice == 'n')
+			{
+				cout << "\n:::::::::END OF PROGRAM::::::::::::\n" << endl;
+				break;
+			}
+			else
+			{
+				cout << "\n\tInvalid input. Please Check!.\n" << endl;
+				continue;
+			}
+		}
+
+	}
 	return 0;
 }
 
